8a34e15f7ff1a1eea0e1474cefb705fa15494f4c
elisiomualumene <elisiomualumene@gmail.com>
2023-12-03 22:00:50 +0100

    update src/components/select/index.tsx

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,38 +1,71 @@
 import { Controller } from "react-hook-form";
 import { SelectInputProps } from "./types";
 
+interface NestedOption {
+  label: string;
+  options?: NestedOption[];
+}
+
+interface NestedSelectProps extends SelectInputProps {
+  options: NestedOption[];
+}
+
 export function Select({
   label,
   control,
   name,
   error,
+  options,
   children,
   ...rest
-}: SelectInputProps): JSX.Element {
+}: NestedSelectProps): JSX.Element {
+  const flattenedOptions: NestedOption[] = [];
+
+  const flattenOptions = (nestedOptions: NestedOption[], prefix = "") => {
+    nestedOptions.forEach((option) => {
+      flattenedOptions.push({
+        label: prefix + option.label,
+      });
+
+      if (option.options) {
+        flattenOptions(option.options, `${prefix} -`);
+      }
+    });
+  };
+
+  flattenOptions(options);
+
   return (
     <div className="w-full">
-      {label && <label className="font-bold indent-2 mb-2">{label}</label>}
+      {label && <label className="font-bold mb-2">{label}</label>}
       <Controller
         name={name}
         control={control}
-        defaultValue=""
+        defaultValue={[]}
         render={({ field: { onBlur, onChange, ref } }) => (
           <select
             {...rest}
             onChange={onChange}
             onBlur={onBlur}
-            defaultValue=""
+            defaultValue={[]}
             ref={ref}
-            className="w-full border outline-none border-[#C5CACF] rounded-xl min-h-[50px] indent-2 font-bold"
+            multiple
+            size={5}
+            className="w-full border outline-none border-[#C5CACF] rounded-xl min-h-[50px] font-bold"
           >
             <option value="" disabled hidden>
-              Select a Sector
+              {label ? `Select ${label.toLowerCase()}` : "Select option(s)"}
             </option>
-            {children}
+            {flattenedOptions.map((option, index) => (
+              <option key={index} value={index}>
+                {option.label}
+              </option>
+            ))}
           </select>
         )}
       />
-      {error && <label className="text-red-500">{error}</label>}
+      {error && <label className="text-red-500">{error.toString()}</label>}
+      {children}
     </div>
   );
 }
