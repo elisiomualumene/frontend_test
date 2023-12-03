000a0f4063376af6ba8a5b3c1816a577703c9535
elisiomualumene <elisiomualumene@gmail.com>
2023-12-03 22:00:42 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import {
-  FormValidationSchema,
-  IndustryOption,
-  Input,
-  Select,
-  industries,
-} from "./module";
+import { FormValidationSchema, Input, Select, environment } from "./module";
+import { useQuery } from "react-query";
 
 export type IFormValues = {
   name: string;
@@ -23,13 +18,17 @@ export default function App() {
     resolver: yupResolver(FormValidationSchema),
   });
 
+  const { isLoading, data: industries } = useQuery("industries data", () =>
+    fetch(`${environment.API_URL}/industries`).then((res) => res.json())
+  );
+
   const SetOnDatabase: SubmitHandler<IFormValues> = (data) => {
     console.log(data);
   };
 
   return (
-    <main className="bg-gradient-to-r from-[#77C3FD] via-[#6D7CFD] to-[#B3AAFC] h-screen w-full font-inter flex items-center justify-center">
-      <article className="bg-white min-w-[420px] max-w-[420px] min-h-[520px] rounded-[30px] p-4 shadow-xl">
+    <main className="bg-zinc-50 h-screen w-full font-inter flex items-center justify-center ">
+      <article className="bg-white w-[420px] min-h-[440px] rounded-[30px] p-4 shadow-xl">
         <p className="text-[#8B96A0] text-center text-[18px]">
           Please enter your name and pick the Sectors you are currently involved
           in.
@@ -37,7 +36,7 @@ export default function App() {
 
         <form
           onSubmit={handleSubmit(SetOnDatabase)}
-          className="flex flex-col gap-4 mt-12"
+          className="flex flex-col gap-4 mt-6"
         >
           <Input
             type="text"
@@ -46,24 +45,16 @@ export default function App() {
             label="Name"
             error={errors?.name?.message}
           />
-          <Select
-            name="sector"
-            control={control}
-            label="Sector"
-            error={errors?.term?.message}
-          >
-            {industries ? (
-              industries.map((option: IndustryOption) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))
-            ) : (
-              <option value="" disabled>
-                void
-              </option>
-            )}
-          </Select>
+          {industries && !isLoading && (
+            <Select
+              name="sector"
+              control={control}
+              label="Sector"
+              error={errors?.sector?.message}
+              options={industries}
+              children={undefined}
+            />
+          )}
 
           <div className="flex flex-row items-center gap-2">
             <Input
@@ -73,7 +64,9 @@ export default function App() {
               className="w-6 h-4 p-0 m-0"
               error={errors?.term?.message}
             />
-            <p>Agree to Terms</p>
+            <p className="text-[#8B96A0] text-center text-[14px]">
+              Agree to Terms
+            </p>
           </div>
           <button className="bg-[#6375F0] min-h-[50px] rounded-xl font-medium text-lg text-white hover:bg-[#6375E0] mt-6">
             Save
