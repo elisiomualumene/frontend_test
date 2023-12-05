a2974fe2a8f2663dfc014df757191cc85b08a90f
elisiomualumene <elisiomualumene@gmail.com>
2023-12-05 23:38:56 +0100

    update formValidation.ts

--- src/utils/forms/formValidation.ts
+++ src/utils/forms/formValidation.ts
@@ -3,5 +3,5 @@ import * as y from "yup";
 export const FormValidationSchema = y.object({
   name: y.string().nonNullable().required("Name is required"),
   sector: y.string().required("Sector is Required").nonNullable(),
-  term: y.bool().required("You need to acept the terms"),
+  terms: y.bool().required("You need to acept the terms"),
 });
